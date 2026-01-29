72db2b9494092418479eb7ab34ad3ef647db76d4
DESKTOP-M5S11J4 <titilayodavid405@gmail.com>
2026-01-29 14:13:48 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+    static void main() {
+        ArrayList<String> myArr = new ArrayList<>();
+
+        myArr.add("David");
+        myArr.add("Oluwapelumi");
+
+    }
+}
